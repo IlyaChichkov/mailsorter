e5b5903d65954927323e4cb073a84db5de86b176
IlyaChichkov <ilya555chich@yandex.ru>
2020-09-12 13:41:23 +0300

    changeee

--- Assets/Scripts/Conveyor/PointEvent.cs
+++ Assets/Scripts/Conveyor/PointEvent.cs
@@ -30,7 +30,7 @@ public class PointEvent : MonoBehaviour
         if((int)triggerObject.GetComponent<MailMarker>().markerColor == (int)color){
             Debug.Log("Right delivery");
         }else{
-            Debug.Log("Wrong delivery df");
+            Debug.Log("Wrong delivery");
         }
         triggerObject.GetComponent<Animator>().SetBool("Destroyed", true);
     }
